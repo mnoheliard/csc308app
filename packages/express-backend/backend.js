a0286ef3f42b2e17184ebe9beecda3e26d6ae6c6
mnoheliard <marthardgzn@gmail.com>
2026-09-08 19:09:08 -0700

    lab2 step 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -52,6 +52,17 @@ const findUserByName = (name) => {
   );
 };
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
