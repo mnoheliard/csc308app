fb5aabb20822d46011a37e78812cc6a5a0004b58
mnoheliard <marthardgzn@gmail.com>
2026-09-19 23:58:58 -0700

    all the steps and reflection

--- tdd/stock-portfolio.js
+++ tdd/stock-portfolio.js
@@ -4,8 +4,27 @@ class Portfolio {
 	{
 		this.stocks = [];
 	}
+	
+	#findSymbol(symbol)
+        {
+                const findSymbol = this.stocks.findIndex(tuple => tuple[0] == symbol);
+                return findSymbol;
+        }
 
-	getShares(){
+	getStocks(symbol){
+		if(symbol != undefined)
+		{
+			const index = this.#findSymbol(symbol);
+			if(index != -1)
+			{
+				return this.stocks[index][1];
+			}
+			else 
+			{
+				return 0;
+			}
+		}
+
 		return this.stocks;
 	}
 	
@@ -21,10 +40,10 @@ class Portfolio {
 	}
 	
 	purchase(symbol, num) {	
-		 const findSymbol = this.stocks.findIndex(tuple => tuple[0] == symbol);
-                if(findSymbol != -1)
+		 const index = this.#findSymbol(symbol);
+                if(index != -1)
                 { 
-                        this.stocks[findSymbol][1] += num;
+                        this.stocks[index][1] += num;
 			
                 }
                 else
@@ -36,21 +55,30 @@ class Portfolio {
 
 	sale(symbol, num)
 	{
-		const findSymbol = this.stocks.findIndex(tuple => tuple[0] == symbol);
-		if(findSymbol != -1)
-		{
-			if(this.stocks[findSymbol][1] >= num) {
-				this.stocks[findSymbol][1] -= num;
-		
-			}
-			else{
-				throw new Error('you dont have that amount');
-			}
-		}
-		else
+
+		const index = this.#findSymbol(symbol);
+		if(index <= -1)
 		{
 			throw new Error('symbol does not exist');
 		}
+
+		else if(index != -1)
+		{
+			if(this.stocks[index][1] >= num) {
+
+				this.stocks[index][1] -= num;
+				if(this.stocks[index][1] == 0)
+				{
+					this.stocks.splice(index, 1);
+				}
+
+		
+			}
+			else{
+				throw new Error('Not possible to sell this number of shares.');
+			}
+		}
+		
 	}
 
 	getNumStocks()
@@ -62,6 +90,21 @@ class Portfolio {
 
 
 }
-
+/*Reflection
+ * While completing this assignment, I was able to follow most of the TDD cycle.
+ * I did write test cases before completing the task, and I did try to refactor 
+ * before moving on but, I would sometimes move on from a task
+ * before refactoring. Once I had moved on from a task and had more information I would go back
+ * to refactor it. However, it wasn't the best move because when I did update it, I would then have to 
+ * update everything else so they wouldn't fail their test.
+ *
+ * After practicing TDD, I thought it was a great way to first test out how you
+ * want your program to work and give you reminders on what you should include
+ * It gave me a more clear mind on what I need to code and how I should do it
+ * before I even started the coding the class. I will continue to try and use this approach
+ * as I complete other assignments and projects.
+ *
+ * 
+ * */
 
 module.exports = Portfolio;
--- tdd/stock-portfolio.test.js
+++ tdd/stock-portfolio.test.js
@@ -4,7 +4,7 @@ describe('Portfolio class', () => {
 	test('2.1 testing portfolio set up works', () => {
 		const portfolio = new Portfolio();
 
-		expect(portfolio.getShares()).toEqual([]);
+		expect(portfolio.getStocks()).toEqual([]);
 	});
 
 	test('2.2 checking whether portfolio checks if empty shares', () => {
@@ -15,32 +15,26 @@ describe('Portfolio class', () => {
 	test('2.3 a purschase does update the portfolio', () => {
 		const portfolio = new Portfolio();
 		portfolio.purchase("adobe", 5);
-		expect(portfolio.getShares()).toEqual([["adobe", 5]]);
+		expect(portfolio.getStocks()).toEqual([["adobe", 5]]);
 	});
 	test('2.3 a purschase, but symbol exists already', () => {
 		const portfolio = new Portfolio();
                 portfolio.purchase("adobe", 5);
 		portfolio.purchase("adobe", 5);
-                expect(portfolio.getShares()).toEqual([["adobe", 10]]);
+                expect(portfolio.getStocks()).toEqual([["adobe", 10]]);
         });
 
 	test('2.4 a sale, substracts the shares from the symbol', () => {
 		const portfolio = new Portfolio();
 		portfolio.purchase("adobe", 5);
 		portfolio.sale("adobe", 2);
-		expect(portfolio.getShares()).toEqual([["adobe", 3]]);
+		expect(portfolio.getStocks()).toEqual([["adobe", 3]]);
 	});
 	test('2.4 a sale, but that sumbol doenst exist', () => {
 		const portfolio = new Portfolio();
                 portfolio.purchase("notAdobe", 5);
                 
                 expect(() => portfolio.sale("adobe", 2)).toThrow('symbol does not exist');
-	});
-	test('2.4 a sale, but the amount being sold is not enough', () => {
-		 const portfolio = new Portfolio();
-                portfolio.purchase("adobe", 5);
-
-                expect(() => portfolio.sale("adobe", 6)).toThrow('you dont have that amount');
 	});
 
 	test('2.5 get the amount of stocks', () => {
@@ -49,5 +43,35 @@ describe('Portfolio class', () => {
 		portfolio.purchase("RBLX", 10);
 		expect(portfolio.getNumStocks()).toEqual(2);
 	});
+
+	test('2.6 check that empty shares are not in stock list', () => {
+		const portfolio = new Portfolio();
+                portfolio.purchase("GMR", 5);
+                portfolio.purchase("RBLX", 10);
+		portfolio.sale("RBLX", 10);
+                expect(portfolio.getStocks()).toEqual([["GMR", 5]]);
+	});
+
+	test('2.7 how many shares a certain symbol has', () => {
+		const portfolio = new Portfolio();
+		portfolio.purchase("GMR", 5);
+		portfolio.purchase("RBLX", 5);
+		expect(portfolio.getStocks("RBLX")).toEqual(5);
+	});
+	test('2.7 how many shares for a symbol doesnt exist', () => {
+		const portfolio = new Portfolio();
+		portfolio.purchase("GRM", 5);
+		expect(portfolio.getStocks("RBLX")).toEqual(0);
+	});
+
+	 test('2.8 a sale, but the amount being sold is not enough', () => {
+                 const portfolio = new Portfolio();
+                portfolio.purchase("adobe", 5);
+
+                expect(() => portfolio.sale("adobe", 6)).toThrow('Not possible to sell this number of shares.');
+        });
+
+
+	
 
 });
